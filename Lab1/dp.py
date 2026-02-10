017ec576a7c374373c38b06136a4d3e420abdb29
kynexi <184446386+kynexi@users.noreply.github.com>
2026-02-10 23:26:09 +0200

    Add dynamic programming method

---
+++ Lab1/dp.py
@@ -0,0 +1,13 @@
+def fib(n):
+    if n <= 1:
+        return n
+
+    prev2 = 0
+    prev1 = 1
+
+    for _ in range(2, n + 1):
+        curr = prev1 + prev2
+        prev2 = prev1
+        prev1 = curr
+
+    return prev1
